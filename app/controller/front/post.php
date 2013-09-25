d04849bf0bb2ed1c2cdb9516577b78eee200a604
Martin Wyatt <martin.wyatt@gmail.com>
2013-09-25 19:48:26 +0100

    Update post.php

--- app/controller/front/post.php
+++ app/controller/front/post.php
@@ -15,7 +15,7 @@ class Controller_Front_Post extends Controller
 
 
 	public function index() {
-		$content = new Model_content($this->database, $this->config);
+		$content = new model_content($this->database, $this->config);
 		if ($this->config->getUrl(1)) {
 			if (! $content->read('post', false, $this->getId($this->config->getUrl(1)))) {
 				$this->route('404');
