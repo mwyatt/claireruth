f9c8b51490ce8f5e6bc9ac4e474938770d243d07
mwyatt <martin.wyatt@gmail.com>
2013-09-26 17:31:36 +0100

    error handler

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @package	eastlancstt
+ * @package	claireruth
  * @author 	Martin Wyatt <martin.wyatt@gmail.com> 
  * @version	0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
