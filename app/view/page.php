ba1f35ff8659b402e7e2b1de6cf22b959921b62e
mwyatt <martin.wyatt@gmail.com>
2013-10-01 12:41:51 +0100

    C:/Users/Martin/AppData/Local/GitHub/PortableGit_015aa71ef18c047ce8509ffb2f9e4bb0e3e73f13/page/ patch

--- app/view/page.php
+++ app/view/page.php
@@ -1,4 +1,4 @@
-<?php require_once($this->pathView() . 'header.php'); ?>
+<?php require_once($this->pathView('header')); ?>
 
 <article class="content page">
 	<header>
@@ -8,9 +8,6 @@
 	<section>
 		<?php echo $this->get('model_content', 'html') ?>
 	</section>
-	<footer>
-		Share this page: <strong>twitter</strong> <strong>facebook</strong>
-	</footer>
 </article>
 
-<?php require_once($this->pathView() . 'footer.php'); ?>
+<?php require_once($this->pathView('footer')); ?>
