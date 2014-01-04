a6e91fb5984288f2a9541fb35edf6f92b46b99a6
mwyatt <martin,wyatt@gmail.com>
2014-01-04 14:00:04 +0000

    js config

---
+++ js/global/config.js
@@ -0,0 +1,21 @@
+var config = {
+        site: 'claire-ruth',
+        content: '',
+        documentBody: '',
+        url~~~: {
+                base: '/',
+                admin: '/',
+                admin_ajax: '/',
+                ajax: '/',
+        },
+        spinner: '<div class="spinner is-tall"></div>',
+        setup: function() {
+                config.content = $('.content');
+                config.documentBody = $(document.body);
+                config.url~~~.base = $('body').data('url~~~-base');
+                config.url~~~.ajax = config.url~~~.base + 'ajax.php'
+                config.url~~~.admin = config.url~~~.base + 'administration/';
+                config.url~~~.adminAjax = config.url~~~.admin + 'ajax.php';
+                config.url~~~.currentNoQuery = [location.protocol, '//', location.host, location.pathname].join('');
+        },
+};
