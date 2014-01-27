b439ddbfb96b35a86543dd291fad2be74694bcca
mwyatt <martin.wyatt@gmail.com>
2014-01-27 17:31:45 +0000

    molds introduced

---
+++ app/class/mold/content/meta.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package	~unknown~
+ * @author Martin Wyatt <martin.wyatt@gmail.com> 
+ * @version	0.1
+ * @license http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+class Mold_Content_Meta extends Config
+{
+
+
+	public $id;
+	public $name;
+	public $value;
+}
