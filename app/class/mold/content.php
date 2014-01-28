04093ec6281bf4efdd376f25e878729c2e292659
mwyatt <martin.wyatt@gmail.com>
2014-01-28 17:32:00 +0000

    molds

--- app/class/mold/content.php
+++ app/class/mold/content.php
@@ -6,11 +6,10 @@
  * @version	0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  */
-class Mold_Content_Meta
+class Mold_Content
 {
 
 
 	public $id;
 	public $name;
-	public $value;
 }
